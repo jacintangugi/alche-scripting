7bd2d792c8604e7ead643ce18fdb02df14d6b042
jacintangugi <j.ngugi@alustudent.com>
2026-08-21 06:38:43 +0000

    Revert top_ten to single-request pattern matching other scripts

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,29 +9,25 @@ def top_ten(subreddit):
     Args:
         subreddit (str): the name of the subreddit.
     """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "alche_api_advanced:v1.0 (by /u/jacintangugi)"}
     params = {"limit": 10}
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False)
 
-    for domain in ("https://www.reddit.com", "https://old.reddit.com"):
-        url = "{}/r/{}/hot.json".format(domain, subreddit)
-        try:
-            response = requests.get(
-                url, headers=headers, params=params,
-                allow_redirects=False, timeout=10)
-        except requests.exceptions.RequestException:
-            continue
+    if response.status_code != 200:
+        print(None)
+        return
 
-        if response.status_code != 200:
-            continue
+    try:
+        posts = response.json().get("data", {}).get("children", [])
+    except ValueError:
+        print(None)
+        return
 
-        try:
-            posts = response.json().get("data", {}).get("children", [])
-        except ValueError:
-            continue
+    if not posts:
+        print(None)
+        return
 
-        if posts:
-            for post in posts:
-                print(post.get("data", {}).get("title"))
-            return
-
-    print(None)
+    for post in posts:
+        print(post.get("data", {}).get("title"))
